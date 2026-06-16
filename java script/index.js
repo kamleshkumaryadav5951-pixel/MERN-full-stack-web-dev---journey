f22c1abdf9b2bbbb1d25b7f7f828b3ad81357206
kamlesh kumar yadav <kamleshkumaryadav5951@gmail.com>
2026-06-16 14:15:32 +0530

    june 16: variables and datatype in JS

---
+++ java script/index.js
@@ -0,0 +1,25 @@
+// var age= 24;
+// if(true){
+//     console.log(age);
+// }
+
+// function solve(){
+//     var age = 24;
+//     console.log(age);
+// }
+// solve()
+// {
+// let a = 10;
+// console.log(a);
+// }
+
+// let a = 10;
+// a = "kamlesh";
+// a = true;
+// a = null;
+
+const a = 20;
+console.log(a);
+a = 28;
+console.log(a);
+
