9296be774eefe836eb53d97ec0a7ee896d563c52
kamlesh kumar yadav <kamleshkumaryadav5951@gmail.com>
2026-06-17 15:12:15 +0530

    loops and string

---
+++ java script/index2.js
@@ -0,0 +1,62 @@
+
+// for(let i = 0; i < 6; i++ ){
+//     if(i == 4){
+//         break;
+//     }
+//     else{
+//         console.log(i);
+//     }
+// }
+
+// for(let i=0; i<=6; i++){
+//     if(i == 3){
+//         continue;
+//     }
+//     else{
+//         console.log(i);
+//     }
+// }
+
+// let i = 1;
+
+// while (i < 6) {
+//     console.log('inside the loop');
+
+//     if (i == 3) {
+//         i++;
+//         continue;
+//     } else {
+//         console.log('hii');
+//     }
+
+//     i++;
+// }
+
+// let Firstname = 'kamlesh';
+// let lastename = 'yadav';
+// let name = `kamlesh
+// kumar
+// yadav`
+// // console.log(typeof(lastname));
+// console.log(name);
+
+// let firstname = new String("kamlesh");
+// console.log(firstname);
+
+// let opt1 = 'English';
+// let opt2 = 'Hindi';
+
+// let FinalAns = ` ${opt1} ${opt2}`;
+// console.log(FinalAns);
+
+// console.log(opt1.toUpperCase());
+
+// let str = "kamlesh";
+// console.log(str.substring(2,4));
+
+// let sentence = " hello world I am kamlesh";
+// let word = sentence.split(' ');
+// console.log(word);
+
+let sentence = "hellojee \"kaise\" ho saare";
+console.log(sentence)
---
+++ java script/tempCodeRunnerFile.js
@@ -0,0 +1,10 @@
+    console.log('inside the loop');
+    if(i ==3){
+        i++;
+        continue;
+    }
+    else
+        console.log('hii');
+    i++;
+    
+}
