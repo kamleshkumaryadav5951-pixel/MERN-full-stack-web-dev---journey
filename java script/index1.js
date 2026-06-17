22dd26b0798ad546ba1c21e0317e3c7c8fb58bf2
kamlesh kumar yadav <kamleshkumaryadav5951@gmail.com>
2026-06-17 13:40:10 +0530

    june 17: operator & conditions

---
+++ java script/index1.js
@@ -0,0 +1,44 @@
+// console.log("kamlesh")
+
+//arithmetic operation
+// let b = 3;
+// let a = 2;
+// console.log(a**b);
+
+// let a = 6;
+// let b = 7;
+
+// // a = a + 7;
+
+
+// console.log( 5 != 5);
+
+// let age = 10;
+// let criteria = (age > 18) ? "I can vote" : "I can't vote"
+// console.log(criteria);
+
+// let ans = (true && false && true);
+
+
+// let ans = (true || false || false);
+// let age = 10;
+// if(age > 18){
+//     console.log("eligble to vote");
+// }
+// else{
+//     console.log("not eligble to vote");
+// }
+
+// 
+
+let number = 3;
+switch(number){
+    case 1: console.log('A');
+    break;
+    case 2: console.log('B');
+    break;
+    case 3: console.log('C');
+    break;
+    case 4: console.log('D');
+    break;
+}
