0b8e9726baa5574c41a1a084a30a3f6ab7038ac8
kamlesh kumar yadav <kamleshkumaryadav5951@gmail.com>
2026-06-24 13:33:35 +0530

    default

---
+++ java script/index 8.js
@@ -0,0 +1,38 @@
+//DEFAULT PARAMETER
+
+
+// function sayname(myname = "kartik yadav"){
+//     console.log('my name is',myname)
+// }
+// sayname('kamlesh');
+
+
+
+// function SayName(fName = "kartik",lName = "yadav"){
+//     console.log('fullname:',fName," ",lName)
+// }
+// SayName();
+
+
+// function SayName(fName = "kartik", lName = fName.toUpperCase()){
+//     console.log('my name is ',fName," ",lName)
+// }
+// SayName();
+
+
+// function solve(value = {age:12, wt:80, ht:170} ){
+//     console.log('KAMLESH', value)
+// }
+// solve();
+
+
+// function solve(value = [12,2,3,55,4] ){
+//     console.log('KAMLESH', value)
+// }
+// solve();
+
+
+function solve(value = 'kartik' ){
+    console.log('KAMLESH', value)
+}
+solve(undefined); // kartik
