fa883f3e65427c94a355016ef064bd81b3bf5856
kamlesh kumar yadav <kamleshkumaryadav5951@gmail.com>
2026-06-24 14:16:01 +0530

    date

---
+++ java script/index 10.js
@@ -0,0 +1,8 @@
+// let curr = new Date()
+// console.log(curr)
+
+let date =new Date('june 20 1998 07:14');
+let newDate = new Date(1998 , 5, 20 ,7)
+
+console.log(date)
+console.log(newDate);
---
+++ java script/index 9.js
@@ -0,0 +1,13 @@
+console.log('kamlesh')
+
+console.log(Math.PI)
+
+console.log(Math.max(20,70,10,24))
+
+console.log(Math.min(20,70,10,24))
+
+console.log(Math.round(2.4))
+
+console.log(Math.ceil(1.4))
+
+console.log(Math.abs(-2))
